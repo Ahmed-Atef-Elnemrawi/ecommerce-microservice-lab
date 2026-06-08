69db2e8cd83efcbf158fae8842151d70d7c34b1c
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-08 23:11:53 +0300

    feat(Application): add ProductType query and handlers

---
+++ Services/Catalog/Catalog.Application/Handlers/Queries/GetAllProductTypesQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers.Queries;
+
+public class GetAllProductTypesQueryHandler(IProductTypeRepository productTypeRepository, IMapper mapper)
+  : IRequestHandler<GetAllProductTypesQuery, IList<ProductTypeDto>>
+{
+  private readonly IProductTypeRepository _productTypeRepository = productTypeRepository;
+  private readonly IMapper _mapper = mapper;
+
+  public async Task<IList<ProductTypeDto>> Handle(GetAllProductTypesQuery request, CancellationToken cancellationToken)
+  {
+    var productTypeList = await _productTypeRepository.GetAllProductTypes();
+    return _mapper.Map<IList<ProductTypeDto>>(productTypeList);
+  }
+}
