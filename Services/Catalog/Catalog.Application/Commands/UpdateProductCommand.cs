6701a2525651fb669540e272c0df188e46a7525f
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-09 15:49:53 +0300

    feat(application): add product update command and handler.

---
+++ Services/Catalog/Catalog.Application/Commands/UpdateProductCommand.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using MediatR;
+
+namespace Catalog.Application.Commands;
+
+public record UpdateProductCommand(
+  string Name,
+  string Description,
+  string Summary,
+  decimal Price,
+  ProductType Type,
+  ProductBrand Brand
+  ) : IRequest<bool>;
---
+++ Services/Catalog/Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers.Commands;
+
+public class UpdateProductCommandHandler(IProductRepository productRepository, IMapper mapper)
+  : IRequestHandler<UpdateProductCommand, bool>
+{
+  public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+  {
+    var productEntity = mapper.Map<Product>(request);
+    var isUpdated = await productRepository.UpdateProduct(productEntity, cancellationToken);
+
+
+    return isUpdated;
+  }
+}
