3fe328ff128fd7041e2222aec43c01400e884354
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-09 19:03:59 +0300

    chore(application): register application layer services
    - configure AutoMapper profiles
    - register MediatR handlers

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplicationServices();
 builder.Services.AddSwaggerGen(options =>
 {
   options.SwaggerDoc("v1", new OpenApiInfo
---
+++ Services/Catalog/Catalog.Application/ServiceRegistration/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Catalog.Application.Mappers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Application.ServiceRegistration;
+
+public static class ApplicationServiceRegistration
+{
+   public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+   {
+      services.AddAutoMapper(options =>
+      {
+         options.AddProfile<CatalogMappersProfile>();
+      });
+
+      services.AddMediatR(options =>
+      {
+         options.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+      });
+
+      return services;
+   }
+}
