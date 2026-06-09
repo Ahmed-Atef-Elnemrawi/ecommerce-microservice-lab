9cd1d3a6da2e215acd76212aba7f908708db4751
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-09 15:43:47 +0300

    feat(application): add product create command and handler.

---
+++ Services/Catalog/Catalog.Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using MediatR;
+
+namespace Catalog.Application.Commands;
+
+public record CreateProductCommand(
+  string Name,
+  string Description,
+  string Summary,
+  decimal Price,
+  ProductType Type,
+  ProductBrand Brand) : IRequest<ProductDto>;
---
+++ Services/Catalog/Catalog.Application/Handlers/Commands/CreateProductCommandHandlers.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers.Commands;
+
+public class CreateProductCommandHandlers(IProductRepository productRepository, IMapper mapper)
+  : IRequestHandler<CreateProductCommand, ProductDto>
+{
+  public async Task<ProductDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+  {
+    var productEntity = mapper.Map<Product>(request);
+    var newProduct = await productRepository.CreateProduct(productEntity, cancellationToken);
+    var productDto = mapper.Map<ProductDto>(newProduct);
+
+    return productDto;
+  }
+}
