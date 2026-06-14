e387a2b08a287b30d0ba45c936cf3f86661d5466
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-14 22:28:25 +0300

    feat(application): introduce ICatalogDbContext abstraction

---
+++ Services/Catalog/Catalog.Application/Common/Interfaces/ICatalogDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Application.Common.Interfaces;
+
+public interface ICatalogDbContext
+{
+  IMongoCollection<Product> Products { get; }
+  IMongoCollection<ProductBrand> ProductBrands { get; }
+  IMongoCollection<ProductType> ProductTypes { get; }
+}
