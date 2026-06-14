fcb04756bdba0423ed42279ecc1238ee4b60bef2
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-14 22:22:24 +0300

    feat(application): introduce product mapping extensions
    - add MapToProductDto extension method for mapping domain entity to DTO

---
+++ Services/Catalog/Catalog.Application/Features/Products/Shared/MappingsExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Application.Features.Products.Shared;
+
+public static class MappingsExtensions
+{
+   public static ProductDto MapToProductDto(this Product product)
+   {
+     return new ProductDto
+     {
+       Id = product.Id,
+       Name = product.Name,
+       Description = product.Description,
+       Summary =  product.Summary,
+       Price = product.Price,
+       Brand = product.Brand.MapToProductBrandDto(),
+       Type = product.Type.MapToProductTypeDto(),
+     };
+   }
+
+   private static ProductBrandDto MapToProductBrandDto(this ProductBrand brand)
+   {
+     return new ProductBrandDto
+     {
+       Id = brand.Id,
+       Name = brand.Name,
+     };
+   }
+
+
+   private static ProductTypeDto MapToProductTypeDto(this ProductType productType)
+   {
+     return new ProductTypeDto
+     {
+        Id = productType.Id,
+        Name = productType.Name,
+     };
+   }
+}
