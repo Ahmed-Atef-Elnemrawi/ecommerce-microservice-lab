79a97233d8499ec4baf49d915ad534a8ca22687d
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-14 19:44:32 +0300

    refactor(application): organize GetProducts Query and handler into a vertical slice
    - rename GetAllProductQuery to GetProductsQuery Query and Handler
    - move GetAllProductQuery and GetAllProductQueryHandler to Application.Products.GetProducts namespace
    
    BREAKING CHANGE: GetProductsQuery and GetProductsQueryHandler are now located under Application.Products.GetProducts namespace

---
+++ Services/Catalog/Catalog.Application/Features/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Application.Common.Interfaces;
+using Catalog.Application.Features.Products.Shared;
+using MediatR;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace Catalog.Application.Features.Products.GetProducts;
+
+public class GetProductsQueryHandler(ICatalogDbContext catalogDbContext)
+  : IRequestHandler<GetProductsQuery, IList<ProductDto>>
+{
+  // private readonly IProductRepository _productRepository = productRepository;
+  // private readonly IMapper _mapper = mapper;
+  private readonly ICatalogDbContext _catalogDbContext = catalogDbContext;
+
+  public async Task<IList<ProductDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+  {
+    // var productList = await _productRepository.GetAllAsync(cancellationToken);
+    // var  productListDto = _mapper.Map<IList<ProductDto>>(productList);
+    //
+    // return productListDto;
+
+    return await _catalogDbContext.Products.AsQueryable().Select(p => p.MapToProductDto())
+      .ToListAsync(cancellationToken);
+  }
+}
--- Services/Catalog/Catalog.Application/Queries/GetAllProductQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using Catalog.Application.Responses;
-using MediatR;
-
-namespace Catalog.Application.Queries;
-
-public record GetAllProductsQuery :  IRequest<IList<ProductDto>>;
