34b7299fbf5f8a7769232a635fbdefdaf15d2565
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-14 19:51:03 +0300

    feat(application): introduce GetProductByName product feature

---
+++ Services/Catalog/Catalog.Application/Features/Products/GetProductsByName/GetProductsByNameQuery.cs
@@ -0,0 +1,6 @@
+﻿using Catalog.Application.Features.Products.Shared;
+using MediatR;
+
+namespace Catalog.Application.Features.Products.GetProductsByName;
+
+public record GetProductsByNameQuery(string Name) : IRequest<IList<ProductDto>>;
---
+++ Services/Catalog/Catalog.Application/Features/Products/GetProductsByName/GetProductsByNameQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Application.Common.Interfaces;
+using Catalog.Application.Features.Products.Shared;
+using MediatR;
+using Catalog.Core.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.Application.Features.Products.GetProductsByName;
+
+public class GetProductsByNameQueryHandler(ICatalogDbContext dbContext)
+  : IRequestHandler<GetProductsByNameQuery, IList<ProductDto>>
+{
+  // private readonly IProductRepository _productRepository = productRepository;
+  // private readonly IMapper _mapper = mapper;
+
+  private readonly ICatalogDbContext _dbContext = dbContext;
+
+  public async Task<IList<ProductDto>> Handle(GetProductsByNameQuery request, CancellationToken cancellationToken)
+  {
+    // var product = await _productRepository.SearchByNameAsync(request.Name, cancellationToken);
+    // var  productDto = _mapper.Map<IList<ProductDto>>(product);
+    // return productDto;
+
+    var filter = Builders<Product>.Filter.Regex(p => request.Name, new BsonRegularExpression(request.Name, "i"));
+
+    return await _dbContext.Products.Find(filter)
+      .Project(p => p.MapToProductDto()).ToListAsync(cancellationToken);
+  }
+}
