68ad36edfb08c53740c43e5f9fe087bc16e94f6e
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-16 19:29:04 +0300

    refactor(application): rename ProductTypeDto to TypeDto

--- Services/Catalog/Catalog.Application/Features/Types/GetTypes/TypeDto.cs
+++ Services/Catalog/Catalog.Application/Features/Types/GetTypes/TypeDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.Application.Features.Types.GetTypes;
 
-public record ProductTypeDto
+public record TypeDto
 {
   public required string Id { get; init; }
   public required string Name { get; init; }
