9c2fcf3dc9cf2bbc0960b84fd7ce018061b02db7
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-16 19:13:06 +0300

    refactor(application): rename ProductBrandDto to BrandDto

--- Services/Catalog/Catalog.Application/Features/Brands/GetBrands/BrandDTO.cs
+++ Services/Catalog/Catalog.Application/Features/Brands/GetBrands/BrandDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.Application.Features.Brands.GetBrands;
 
-public record ProductBrandDto
+public record BrandDto
 {
   public required string Id { get; init; }
   public required string Name { get; init; }
