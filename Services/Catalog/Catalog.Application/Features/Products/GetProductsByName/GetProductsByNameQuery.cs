e59785cf1dc66ed171f10625b270daf79d2274ca
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-16 21:10:34 +0300

    feat(application): refactor GetProductsByName and apply result pattern
    - return Result<IList<ProductsDto>> instead of IList<ProductsDto>

--- Services/Catalog/Catalog.Application/Features/Products/GetProductsByName/GetProductsByNameQuery.cs
+++ Services/Catalog/Catalog.Application/Features/Products/GetProductsByName/GetProductsByNameQuery.cs
@@ -1,6 +1,7 @@
-﻿using Catalog.Application.Features.Products.Shared;
+﻿using Catalog.Application.Common.Models;
+using Catalog.Application.Features.Products.Shared;
 using MediatR;
 
 namespace Catalog.Application.Features.Products.GetProductsByName;
 
-public record GetProductsByNameQuery(string Name) : IRequest<IList<ProductDto>>;
+public record GetProductsByNameQuery(string Name) : IRequest<Result<IList<ProductDto>>>;
--- Services/Catalog/Catalog.Application/Features/Products/GetProductsByName/GetProductsByNameQueryHandler.cs
+++ Services/Catalog/Catalog.Application/Features/Products/GetProductsByName/GetProductsByNameQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Catalog.Application.Common.Interfaces;
+using Catalog.Application.Common.Models;
 using Catalog.Application.Features.Products.Shared;
 using MediatR;
 using Catalog.Core.Entities;
@@ -8,14 +9,14 @@ using MongoDB.Driver;
 namespace Catalog.Application.Features.Products.GetProductsByName;
 
 public class GetProductsByNameQueryHandler(ICatalogDbContext dbContext)
-  : IRequestHandler<GetProductsByNameQuery, IList<ProductDto>>
+  : IRequestHandler<GetProductsByNameQuery, Result<IList<ProductDto>>>
 {
   // private readonly IProductRepository _productRepository = productRepository;
   // private readonly IMapper _mapper = mapper;
 
   private readonly ICatalogDbContext _dbContext = dbContext;
 
-  public async Task<IList<ProductDto>> Handle(GetProductsByNameQuery request, CancellationToken cancellationToken)
+  public async Task<Result<IList<ProductDto>>> Handle(GetProductsByNameQuery request, CancellationToken cancellationToken)
   {
     // var product = await _productRepository.SearchByNameAsync(request.Name, cancellationToken);
     // var  productDto = _mapper.Map<IList<ProductDto>>(product);
@@ -23,7 +24,9 @@ public class GetProductsByNameQueryHandler(ICatalogDbContext dbContext)
 
     var filter = Builders<Product>.Filter.Regex(p => request.Name, new BsonRegularExpression(request.Name, "i"));
 
-    return await _dbContext.Products.Find(filter)
+    var productsDto = await _dbContext.Products.Find(filter)
       .Project(p => p.MapToProductDto()).ToListAsync(cancellationToken);
+
+    return  Result<IList<ProductDto>>.Success(productsDto);
   }
 }
