a10ba76246c3b9966e3b8db967dabf061eeec1e3
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-16 21:19:57 +0300

    feat(application): add CreateProduct validation with FluentValidation
    - define validation rules for CreateProduct command

---
+++ Services/Catalog/Catalog.Application/Features/Products/CreateProduct/CreateProductValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+
+namespace Catalog.Application.Features.Products.CreateProduct;
+
+public class CreateProductValidator : AbstractValidator<CreateProductCommand>
+{
+  public CreateProductValidator()
+  {
+    // Name
+    RuleFor(x => x.Name)
+      .NotEmpty()
+      .WithMessage("Name is required");
+
+    // Description
+    RuleFor(x => x.Description)
+      .NotEmpty()
+      .WithMessage("Description is required")
+      .MaximumLength(500)
+      .WithMessage("Description cannot exceed 500 characters");
+
+    // Summary
+    RuleFor(x => x.Summary)
+      .NotEmpty()
+      .WithMessage("Summary is required.")
+      .MaximumLength(300)
+      .WithMessage("Summary cannot exceed 300 characters");
+
+    // Price
+    RuleFor(x => x.Price)
+      .GreaterThan(0)
+      .WithMessage("Price must be greater than 0.");
+
+    // Brand
+    RuleFor(x => x.BrandId)
+      .NotEmpty()
+      .WithMessage("BrandId is required");
+
+    // Type
+    RuleFor(x => x.TypeId)
+      .NotEmpty()
+      .WithMessage("TypeId is required");
+  }
+}
