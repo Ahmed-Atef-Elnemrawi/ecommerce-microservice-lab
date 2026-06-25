eda04aae7aa4041a82cd627b784d19f00daec499
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-25 18:16:50 +0300

    feat(core): add shopping cart repository contract

---
+++ Services/Basket.Core/Repositories/IShoppingCartRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Core.Repositories;
+
+public interface IShoppingCartRepository
+{
+  Task<Cart?> GetByUserNameAsync(string userName, CancellationToken cancellationToken);
+  Task<Cart> CreateAsync(Cart shoppingCart, CancellationToken cancellationToken);
+  Task DeleteAsync(string userName, CancellationToken cancellationToken);
+}
