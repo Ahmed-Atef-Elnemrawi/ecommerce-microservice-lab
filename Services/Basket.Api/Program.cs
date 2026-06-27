95be662c4b47b5f4b19ff11913b1b603524037ab
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-27 14:17:06 +0300

    chore(api): configure project settings

--- Services/Basket.Api/Program.cs
+++ Services/Basket.Api/Program.cs
@@ -1,48 +1,35 @@
-﻿namespace Basket.Api;
+﻿using Asp.Versioning;
+using Basket.Application.ServiceRegistrations;
+using Basket.Infrastructure.ServiceRegistrations;
+using Scalar.AspNetCore;
 
-public class Program
-{
-  public static void Main(string[] args)
-  {
-    var builder = WebApplication.CreateBuilder(args);
+var builder =  WebApplication.CreateBuilder(args);
+ builder.Services.AddControllers();
+ builder.Services.AddOpenApi();
+ 
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices(builder.Configuration);
 
-    // Add services to the container.
-    builder.Services.AddAuthorization();
+ builder.Services.AddApiVersioning(options =>
+ {
+   options.ReportApiVersions = true;
+   options.AssumeDefaultVersionWhenUnspecified = true;
+   options.DefaultApiVersion = new ApiVersion(1, 0);
+   options.ApiVersionReader = new UrlSegmentApiVersionReader();
+ });
+ 
+ var app = builder.Build();
 
-    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-    builder.Services.AddOpenApi();
+ if (app.Environment.IsDevelopment())
+ {
+   app.MapOpenApi();
+   app.MapScalarApiReference(options =>
+   {
+     options.Title = "Basket API";
+   });
+ }
+ 
+ app.UseHttpsRedirection();
+ app.MapControllers();
 
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-      app.MapOpenApi();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    var summaries = new[]
-    {
-      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-      {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            {
-              Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-              TemperatureC = Random.Shared.Next(-20, 55),
-              Summary = summaries[Random.Shared.Next(summaries.Length)]
-            })
-          .ToArray();
-        return forecast;
-      })
-      .WithName("GetWeatherForecast");
-
-    app.Run();
-  }
-}
+ app.Run();
