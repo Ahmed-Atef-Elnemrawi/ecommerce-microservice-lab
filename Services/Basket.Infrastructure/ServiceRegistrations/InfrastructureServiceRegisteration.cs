f2aab8ddf052c55171a50e11b02025af5a129b52
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-27 14:12:18 +0300

    feat(infra): add infrastructure service registration

---
+++ Services/Basket.Infrastructure/ServiceRegistrations/InfrastructureServiceRegisteration.cs
@@ -0,0 +1,30 @@
+﻿using Basket.Core.Repositories;
+using Basket.Infrastructure.Configurations;
+using Basket.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket.Infrastructure.ServiceRegistrations;
+
+public static class InfrastructureServiceRegistration
+{
+  public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+    IConfiguration configuration)
+  {
+    
+    services.AddOptions<CacheSettings>()
+      .BindConfiguration(CacheSettings.SectionName)
+      .ValidateOnStart();
+    
+    services.AddStackExchangeRedisCache(options =>
+    {
+      var cacheSettings = configuration.GetSection(CacheSettings.SectionName).Get<CacheSettings>();
+      options.Configuration =  cacheSettings!.ConnectionString;
+      options.InstanceName = "Basket:";
+    });
+
+    services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+
+    return services;
+  }
+}
