b07202f0dae5ae6ac5b3af2db10ada91ac7cdc19
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-27 14:11:17 +0300

    feat(application): add application service registration

---
+++ Services/Basket.Application/ServiceRegistrations/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Basket.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket.Application.ServiceRegistrations;
+
+public static class ApplicationServiceRegistration
+{
+  public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+    IConfiguration configuration)
+  {
+    services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    
+    services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+    
+    services.AddMediatR(options =>
+    {
+      options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+    });
+
+    return services;
+  }
+}
---
+++ Services/Basket.Infrastructure/Configurations/CacheSettings.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Basket.Infrastructure.Configurations;
+
+public class CacheSettings
+{
+  public const string SectionName = "CacheSettings";
+  public string ConnectionString { get; init; } = string.Empty;
+}
