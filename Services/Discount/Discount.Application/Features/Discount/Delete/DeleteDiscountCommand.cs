51a32ce8f024c627043b3d36a35561a4d56e190b
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-30 21:28:36 +0300

    feat(application): add discount delete command

---
+++ Services/Discount/Discount.Application/Features/Discount/Delete/DeleteDiscountCommand.cs
@@ -0,0 +1,6 @@
+﻿using Discount.Application.Common.Models.ResultModel;
+using MediatR;
+
+namespace Discount.Application.Features.Discount.Delete;
+
+public sealed record DeleteDiscountCommand(int Id) : IRequest<Result<Unit>>;
