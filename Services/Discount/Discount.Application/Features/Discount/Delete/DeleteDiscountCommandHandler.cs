2f68ab34902947fed126c566b01ee3dc368214f1
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-30 21:28:36 +0300

    feat(application): add discount delete command

---
+++ Services/Discount/Discount.Application/Features/Discount/Delete/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Discount.Application.Common.Models.ResultModel;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Features.Discount.Delete;
+
+public class DeleteDiscountCommandHandler(IDiscountRepository discountRepository)
+  : IRequestHandler<DeleteDiscountCommand, Result<Unit>>
+{
+  public async Task<Result<Unit>> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+  {
+    var discount = await discountRepository.GetAsync(request.Id, cancellationToken);
+
+    if (discount is null)
+      return Result<Unit>.Failure("Coupon.NotFound", "Coupon not found", ErrorType.NotFound);
+
+    await discountRepository.DeleteAsync(request.Id, cancellationToken);
+    return Result<Unit>.Success(Unit.Value);
+  }
+}
