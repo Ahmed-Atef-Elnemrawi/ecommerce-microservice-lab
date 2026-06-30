9a0347b15d097e822f2599b20730797bb0bf7ae1
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-30 21:28:06 +0300

    feat(application) add discount update command

---
+++ Services/Discount/Discount.Application/Features/Discount/Update/UpdateDiscountCommand.cs
@@ -0,0 +1,8 @@
+﻿using Discount.Application.Common.Dto;
+using Discount.Application.Common.Models.ResultModel;
+using MediatR;
+
+namespace Discount.Application.Features.Discount.Update;
+
+public sealed record UpdateDiscountCommand(int Id, string ProductId, string Description, int Amount, int Version)
+  : IRequest<Result<CouponDto>>;
---
+++ Services/Discount/Discount.Application/Features/Discount/Update/UpdateDiscountCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Discount.Application.Common.Dto;
+using Discount.Application.Common.Extensions;
+using Discount.Application.Common.Models.ResultModel;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Features.Discount.Update;
+
+public sealed class UpdateDiscountCommandHandler(IDiscountRepository discountRepository)
+  : IRequestHandler<UpdateDiscountCommand, Result<CouponDto>>
+{
+  public async Task<Result<CouponDto>> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+  {
+    var coupon = await discountRepository.GetAsync(request.Id, cancellationToken);
+
+    if (coupon is null)
+      return Result<CouponDto>.Failure("Coupon.NotFound", "Coupon not found", ErrorType.NotFound);
+
+    coupon.UpdateAmount(request.Amount);
+    coupon.ChangeDescription(request.Description);
+
+    var isUpdated = await discountRepository.UpdateAsync(coupon, cancellationToken);
+
+    if (!isUpdated)
+      return Result<CouponDto>.Failure("Update.Conflict",
+        "Coupon was modified by another request", ErrorType.Conflict);
+
+    return Result<CouponDto>.Success(coupon.ToDto());
+  }
+}
