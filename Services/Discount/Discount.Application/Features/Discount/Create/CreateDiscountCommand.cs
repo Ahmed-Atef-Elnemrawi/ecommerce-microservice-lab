d9ecc5e8a2fae4565dc8d768d2ccd402061c2883
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-30 21:27:18 +0300

    feat(application) add discount create command

---
+++ Services/Discount/Discount.Application/Features/Discount/Create/CreateDiscountCommand.cs
@@ -0,0 +1,8 @@
+﻿using Discount.Application.Common.Dto;
+using Discount.Application.Common.Models.ResultModel;
+using MediatR;
+
+namespace Discount.Application.Features.Discount.Create;
+
+public sealed record CreateDiscountCommand(string ProductId, string Description, int Amount)
+  : IRequest<Result<CouponDto>>;
---
+++ Services/Discount/Discount.Application/Features/Discount/Create/CreateDiscountCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Application.Common.Dto;
+using Discount.Application.Common.Extensions;
+using Discount.Application.Common.Models.ResultModel;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Features.Discount.Create;
+
+public sealed class CreateDiscountCommandHandler(IDiscountRepository discountRepository)
+  : IRequestHandler<CreateDiscountCommand, Result<CouponDto>>
+{
+  public async Task<Result<CouponDto>> Handle(CreateDiscountCommand request, CancellationToken cancellationToken)
+  {
+    var newCoupon = Coupon.Create(request.ProductId, request.Description, request.Amount);
+    var isCreated = await discountRepository.CreateAsync(newCoupon, cancellationToken);
+
+    if (!isCreated)
+      return Result<CouponDto>.Failure("Coupon.Exist", "Coupon is already exists", ErrorType.AlreadyExist);
+
+    return Result<CouponDto>.Success(newCoupon.ToDto());
+  }
+}
