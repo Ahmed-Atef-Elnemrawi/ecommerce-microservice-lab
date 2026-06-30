7856f4da010703adbe2f5fbfec7281a4bd9597c0
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-30 21:27:18 +0300

    feat(application) add discount create command

---
+++ Services/Discount/Discount.Application/Features/Discount/Create/CreateDiscountCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Discount.Application.Features.Discount.Create;
+
+public class CreateDiscountCommandValidator : AbstractValidator<CreateDiscountCommand>
+{
+  public CreateDiscountCommandValidator()
+  {
+    RuleFor(x => x.ProductId)
+      .NotEmpty()
+      .WithMessage("ProductId is required");
+
+    RuleFor(x => x.Description)
+      .NotEmpty()
+      .WithMessage("Description is required")
+      .MaximumLength(200)
+      .WithMessage("Description cannot exceed 200 characters");
+
+    RuleFor(x => x.Amount)
+      .GreaterThan(0)
+      .WithMessage("Amount must be greater than 0");
+  }
+}
