1ff9f013c636b50584f9f660379b259155a1d0d9
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-06-30 21:22:54 +0300

    feat(application): setup validation behavior pipleline for MediatR

---
+++ Services/Discount/Discount.Application/ServiceRegistrations/ApplicationServiceRegistrations.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Discount.Application.Common.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using MediatR;
+
+namespace Discount.Application.ServiceRegistrations;
+
+public static class ApplicationServiceRegistrations
+{
+  public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+  {
+    services.AddMediatR(options =>
+    {
+      options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+    });
+    
+    services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+    
+    return services;
+  }
+}
