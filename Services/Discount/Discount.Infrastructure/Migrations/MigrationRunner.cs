eed55b5301798eabd320520734a666f0b8918137
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-07-01 07:24:12 +0300

    feat(infrastructure): add initial database schema and migration runner

---
+++ Services/Discount/Discount.Infrastructure/Migrations/MigrationRunner.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using DbUp;
+
+namespace Discount.Infrastructure.Migrations;
+
+public sealed class MigrationRunner
+{
+  public static void Run(string connectionString)
+  {
+    var upgrader = DeployChanges.To
+      .PostgresqlDatabase(connectionString)
+      .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+      .LogToConsole()
+      .Build();
+    
+    var result = upgrader.PerformUpgrade();
+    
+    if (!result.Successful)
+      throw new Exception("Migration failed", result.Error);
+  }
+}
