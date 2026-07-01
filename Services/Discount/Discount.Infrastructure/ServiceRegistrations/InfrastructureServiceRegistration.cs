7f5fa9fd3181095b544f40bf5aaebb72c4254ce6
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-07-01 07:16:54 +0300

    feat(infrastructure): add database configuration settings

---
+++ Services/Discount/Discount.Infrastructure/ServiceRegistrations/InfrastructureServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Core.Repositories;
+using Discount.Infrastructure.Migrations;
+using Discount.Infrastructure.Persistence.Repositories;
+using Discount.Infrastructure.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Discount.Infrastructure.ServiceRegistrations;
+
+public static class InfrastructureServiceRegistration
+{
+  public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+    IConfiguration configuration)
+  {
+    services.AddOptions<DbSettings>()
+      .Bind(configuration.GetSection(DbSettings.SectionName))
+      .ValidateOnStart();
+    
+    return services;
+  }
+}
