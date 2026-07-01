ccabfb2c73ec37d2025c4e2ed7719175544c9078
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-07-01 07:16:54 +0300

    feat(infrastructure): add database configuration settings

---
+++ Services/Discount/Discount.Infrastructure/Settings/DbSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Discount.Infrastructure.Settings;
+
+public sealed class DbSettings
+{
+  public const string SectionName = "NpgsqlSettings";
+  public string ConnectionString { get; init; } = string.Empty;
+}
