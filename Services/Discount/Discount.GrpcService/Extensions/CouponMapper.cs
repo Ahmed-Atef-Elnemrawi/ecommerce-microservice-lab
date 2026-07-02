564742759451ef329196a1939c6bdf08ea253610
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-07-02 17:35:47 +0300

    feat(service): implement discount gRPC service

---
+++ Services/Discount/Discount.GrpcService/Extensions/CouponMapper.cs
@@ -0,0 +1,18 @@
+﻿using Discount.Application.Common.Dto;
+using Discount.Grpc.Protos;
+
+namespace Discount.GrpcService.Extensions;
+
+public static class CouponMapper
+{
+  public static CouponModel ToProto(this CouponDto result)
+  {
+    return new CouponModel
+    {
+      Id = result.Id,
+      ProductId = result.ProductId,
+      Description = result.Description,
+      Amount = result.Quantity,
+    };
+  }
+}
