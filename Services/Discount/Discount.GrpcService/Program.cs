6a4e1597f0fe11bdc26097da14e21e8700b9ba50
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-07-02 17:30:42 +0300

    feat(service): configure service startup and register dependencies

---
+++ Services/Discount/Discount.GrpcService/Program.cs
@@ -0,0 +1,28 @@
+﻿using Discount.Application.ServiceRegistrations;
+using Discount.GrpcService.Services;
+using Discount.Infrastructure.ServiceRegistrations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+}
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
+app.MapGet("/",
+  () =>
+    """
+      Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client,
+      visit: https://go.microsoft.com/fwlink/?linkid=2086909
+    """);
+
+app.Run();
