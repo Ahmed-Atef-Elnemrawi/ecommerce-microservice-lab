5836440303210d8fd938b40320e9f23592d1a1ae
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-07-15 21:35:50 +0300

    feat(basket): integrate discount service in create cart handler

--- Services/Basket/Basket.Application/Features/ShoppingCart/Create/CreateCartCommandHandler.cs
+++ Services/Basket/Basket.Application/Features/ShoppingCart/Create/CreateCartCommandHandler.cs
@@ -1,13 +1,14 @@
 ﻿using Basket.Application.Common.Dto;
 using Basket.Application.Common.Extensions;
 using Basket.Application.Common.Models.ResultModel;
+using Basket.Application.ExternalServices.Discount;
 using Basket.Core.Entities;
 using Basket.Core.Repositories;
 using MediatR;
 
 namespace Basket.Application.Features.ShoppingCart.Create;
 
-public sealed class CreateCartCommandHandler(IShoppingCartRepository cartRepository)
+public sealed class CreateCartCommandHandler(IShoppingCartRepository cartRepository, IDiscountService discountService)
   : IRequestHandler<CreateCartCommand, Result<CartDto>>
 {
   public async Task<Result<CartDto>> Handle(CreateCartCommand request, CancellationToken cancellationToken)
@@ -18,7 +19,20 @@ public sealed class CreateCartCommandHandler(IShoppingCartRepository cartReposit
     cart.Clear();
 
     foreach (var item in request.CartItemsDto)
+    {
       cart.AddItem(item.ProductId, item.ProductName, item.Quantity, item.Price, item.ImageUrl);
+      // N+1 Problem:
+      // For N cart items → N gRPC calls → N database hits.
+      // This causes unnecessary network overhead and degrades performance.
+      //
+      // Solution:
+      // Use a batch request to retrieve all discounts in a single call.
+      // TODO: Replace with batch discount request
+      var discount = await discountService.GetDiscount(item.ProductId);
+    
+      if (discount is not null)
+        cart.ApplyDiscount(item.ProductId, discount.Amount);
+    }
 
     await cartRepository.CreateAsync(cart, cancellationToken);
 
