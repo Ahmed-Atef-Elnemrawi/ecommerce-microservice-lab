3336e8cb13084be93fda64eec399adf71b3dc76a
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-07-15 21:22:20 +0300

    feat(basket-application): add discount service contract and dto

---
+++ Services/Basket/Basket.Application/ExternalServices/Discount/Dto/DiscountDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Basket.Application.ExternalServices.Discount.Dto;
+
+public sealed record DiscountDto(int Id, string ProductId, string Description, int Amount);
---
+++ Services/Basket/Basket.Application/ExternalServices/Discount/IDiscountService.cs
@@ -0,0 +1,8 @@
+﻿using Basket.Application.ExternalServices.Discount.Dto;
+
+namespace Basket.Application.ExternalServices.Discount;
+
+public interface IDiscountService
+{
+  Task<DiscountDto?> GetDiscount(string productId);
+}
