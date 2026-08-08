cc9610afc973af02dd87dd0b8e76b60c5a654db2
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:11:51 +0300

    feat(ordering): add delete order use case

---
+++ Services/Ordering/Ordering.Application/Features/Ordering/Delete/DeleteOrderCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Ordering.Application.Common.Models.ResultModel;
+using Ordering.Domain.Repositories;
+
+namespace Ordering.Application.Features.Ordering.Delete;
+
+public sealed class DeleteOrderCommandHandler(IOrderRepository orderRepository)
+  : IRequestHandler<DeleteOrderCommand, Result<Unit>>
+{
+  public async Task<Result<Unit>> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+  {
+    var order = await orderRepository.GetByIdAsync(request.OrderId, cancellationToken);
+
+    if (order == null) 
+      return Result<Unit>.Failure(
+        "Order.NotFound",
+        $"Order with id '{request.OrderId}' was not found.",
+        ErrorType.NotFound
+      );
+    
+    await  orderRepository.DeleteAsync(order, cancellationToken);
+    return Result<Unit>.Success(Unit.Value);
+  }
+}
