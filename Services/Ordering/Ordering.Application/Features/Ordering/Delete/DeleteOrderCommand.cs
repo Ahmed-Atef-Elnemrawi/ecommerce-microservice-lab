650be1721dfdef87bdda20eb7bd412efa4887fad
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:11:51 +0300

    feat(ordering): add delete order use case

---
+++ Services/Ordering/Ordering.Application/Features/Ordering/Delete/DeleteOrderCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Ordering.Application.Common.Models.ResultModel;
+
+namespace Ordering.Application.Features.Ordering.Delete;
+
+public sealed record DeleteOrderCommand(int OrderId): IRequest<Result<Unit>>;
