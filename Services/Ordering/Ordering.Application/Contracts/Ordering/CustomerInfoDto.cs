99465cd0f6eda5e69cc23e92f2b731f1eb38e282
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:08:26 +0300

    feat(application): add order contracts and DTOs

---
+++ Services/Ordering/Ordering.Application/Contracts/Ordering/CustomerInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Application.Contracts.Ordering;
+
+public sealed record CustomerInfoDto(
+  string FirstName,
+  string LastName,
+  string Email,
+  string PhoneNumber
+);
---
+++ Services/Ordering/Ordering.Application/Contracts/Ordering/PaymentInfoDto.cs
@@ -0,0 +1,8 @@
+﻿using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Contracts.Ordering;
+
+public sealed record PaymentInfoDto(string CardName,
+  string CardNumber,
+  string CardExpirationDate,
+  PaymentMethods PaymentMethods);
