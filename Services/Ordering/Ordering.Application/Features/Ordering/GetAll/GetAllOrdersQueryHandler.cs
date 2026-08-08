bc7ede442ad2e8bdc8a992828f3a6e6385a4c124
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:09:25 +0300

    feat(ordering): add order get All query use case

---
+++ Services/Ordering/Ordering.Application/Features/Ordering/GetAll/GetAllOrdersQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Ordering.Application.Common.Mappings;
+using Ordering.Application.Common.Models.ResultModel;
+using Ordering.Application.Contracts.Ordering;
+using Ordering.Domain.Repositories;
+
+namespace Ordering.Application.Features.Ordering.GetAll;
+
+public sealed class GetAllOrdersQueryHandler(IOrderRepository orderRepository)
+  : IRequestHandler<GetAllOrdersQuery, Result<IEnumerable<OrderDto>>>
+{
+  public async Task<Result<IEnumerable<OrderDto>>> Handle(GetAllOrdersQuery request, CancellationToken cancellationToken)
+  {
+    var orders = await orderRepository.GetAllAsync(cancellationToken);
+    var ordersDto = orders.Select(o => o.MapToOrderDto()).AsEnumerable();
+    return Result<IEnumerable<OrderDto>>.Success(ordersDto);
+  }
+}
