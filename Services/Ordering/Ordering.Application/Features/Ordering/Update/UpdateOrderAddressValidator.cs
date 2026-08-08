93aae584b8583c3b8d819f14d04c74833008fe30
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:11:31 +0300

    feat(ordering): add update order use case

---
+++ Services/Ordering/Ordering.Application/Features/Ordering/Update/UpdateOrderAddressValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Ordering.Application.Contracts.Ordering;
+
+namespace Ordering.Application.Features.Ordering.Update;
+
+public class UpdateOrderAddressValidator : AbstractValidator<AddressDto>
+{
+  public UpdateOrderAddressValidator()
+  {
+    RuleFor(x => x.AddressLine)
+      .NotEmpty()
+      .MaximumLength(200);
+
+    RuleFor(x => x.Country)
+      .NotEmpty()
+      .MaximumLength(100);
+
+    RuleFor(x => x.City)
+      .NotEmpty()
+      .MaximumLength(100);
+
+    RuleFor(x => x.State)
+      .NotEmpty()
+      .MaximumLength(100);
+
+    RuleFor(x => x.ZipCode)
+      .NotEmpty()
+      .MaximumLength(20);
+  }
+}
