902750f5cc68dc650d3bbdac86c3dc0695149107
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:10:13 +0300

    feat(ordering): add order get by username query use case

---
+++ Services/Ordering/Ordering.Application/Features/Ordering/GetByUserName/GetOrdersByUserNameQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Ordering.Application.Common.Models.ResultModel;
+using Ordering.Application.Contracts.Ordering;
+
+namespace Ordering.Application.Features.Ordering.GetByUserName;
+
+public sealed record GetOrdersByUserNameQuery(string UserName) : IRequest<Result<IEnumerable<OrderDto>>>;
