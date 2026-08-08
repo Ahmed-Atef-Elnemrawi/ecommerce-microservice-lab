f202c940f8ad62e58d6e5e44363f5c20149e6351
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:15:17 +0300

    feat(application): add service registrations

---
+++ Services/Ordering/Ordering.Application/ServiceRegistrations/ApplicationServicesRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application.ServiceRegistrations;
+
+public static class ApplicationServicesRegistrations
+{
+  public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+  {
+    services.AddMediatR(options =>
+    {
+      options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+    });
+    
+    return services;
+  }
+}
