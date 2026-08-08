c4301f3d4ce56c9b6f8a193b4a51ef69fd1601f8
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-08 21:09:25 +0300

    feat(ordering): add order get All query use case

---
+++ Services/Ordering/Ordering.Application/Features/Ordering/GetAll/GetAllOrdersQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Ordering.Application.Common.Models.ResultModel;
+using Ordering.Application.Contracts.Ordering;
+
+namespace Ordering.Application.Features.Ordering.GetAll;
+
+public sealed record GetAllOrdersQuery() : IRequest<Result<IEnumerable<OrderDto>>>;
