bb1446aa945c1e79415b14dcb091ed0c489b04b5
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-11 12:48:36 +0300

    feat(infrastructure): add OrderDbContext design time Factory

---
+++ Services/Ordering/Ordering.Infrastructure/Persistence/context/OrderDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Ordering.Infrastructure.Persistence.Context;
+using Ordering.Infrastructure.Settings;
+
+namespace Ordering.Infrastructure.Persistence.context;
+
+public sealed class OrderDbContextFactory(IConfiguration configuration) 
+  : IDesignTimeDbContextFactory<OrderDbContext>
+{
+  public OrderDbContext CreateDbContext(string[] args)
+  {
+    var optionsBuilder = new DbContextOptionsBuilder<OrderDbContext>();
+
+    optionsBuilder.UseSqlServer(configuration.GetConnectionString(OrderDbSettings.SectionName));
+    
+    return new OrderDbContext(optionsBuilder.Options);
+  }
+}
