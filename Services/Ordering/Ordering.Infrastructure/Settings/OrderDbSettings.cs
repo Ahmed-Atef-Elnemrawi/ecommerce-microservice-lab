17ef3e2a9e56172d345025b6f2dd4d5f6e0425d1
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-11 12:46:30 +0300

    feat(infrastructure): add order db settings option pattern

---
+++ Services/Ordering/Ordering.Infrastructure/Settings/OrderDbSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Infrastructure.Settings;
+
+public class OrderDbSettings
+{
+  public const string SectionName = "OrderingDbSettings";
+  public string ConnectionString { get; set; } = null!;
+}
