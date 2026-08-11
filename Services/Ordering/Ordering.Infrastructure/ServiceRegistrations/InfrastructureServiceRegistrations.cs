519976138b99db5b68b0cb0a7278420a414823a6
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-11 12:50:25 +0300

    feat(ordering-infra): register infrastructure services

---
+++ Services/Ordering/Ordering.Infrastructure/ServiceRegistrations/InfrastructureServiceRegistrations.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Common.interfaces;
+using Ordering.Domain.Repositories;
+using Ordering.Infrastructure.Persistence.Context;
+using Ordering.Infrastructure.Persistence.Initialization;
+using Ordering.Infrastructure.Persistence.Repositories;
+using Ordering.Infrastructure.Settings;
+
+namespace Ordering.Infrastructure.ServiceRegistrations;
+
+public static class InfrastructureServiceRegistrations
+{
+  public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+    IConfiguration configuration)
+  {
+    services
+      .AddOptions<OrderDbSettings>()
+      .BindConfiguration(OrderDbSettings.SectionName)
+      .ValidateOnStart();
+
+    services.AddDbContext<OrderDbContext>(options =>
+    {
+      options.UseSqlServer(configuration.GetConnectionString(OrderDbSettings.SectionName));
+    });
+    
+    services.AddScoped<IPersistenceContext>(sp => sp.GetRequiredService<OrderDbContext>());
+    
+    services.AddScoped<IOrderRepository,  OrderRepository>();
+
+    services.AddHostedService<DatabaseInitializer>();
+    
+    return services;
+  }
+}
