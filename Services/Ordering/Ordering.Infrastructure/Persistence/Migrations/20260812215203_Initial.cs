c79d316be94a79b842516c01c165149516306a94
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-13 22:50:51 +0300

    feat(ordering-infrastructure): add initial database migration and seeding

---
+++ Services/Ordering/Ordering.Infrastructure/Persistence/Migrations/20260812215203_Initial.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CustomerInfo_FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerInfo_LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerInfo_Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerInfo_PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_AddressLine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_ZipCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentInfo_CardName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentInfo_CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentInfo_CardExpirationDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentInfo_PaymentMethods = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
