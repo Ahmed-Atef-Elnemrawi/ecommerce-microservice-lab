b6556724948786c185a81b938e5ffad8ee8c971e
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2026-08-13 22:40:15 +0300

    feat(ordering-api): bootstrap ordering API

---
+++ Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning;
+using Ordering.Application.ServiceRegistrations;
+using Ordering.Infrastructure.ServiceRegistrations;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddApiVersioning(options =>
+{
+  options.ReportApiVersions = true;
+  options.AssumeDefaultVersionWhenUnspecified = true;
+  options.DefaultApiVersion = new ApiVersion(1, 0);
+  options.ApiVersionReader = new UrlSegmentApiVersionReader();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.MapOpenApi();
+  app.MapScalarApiReference(options =>
+  {
+    options.Title = "Order API";
+    options.Theme = ScalarTheme.DeepSpace;
+  });
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
